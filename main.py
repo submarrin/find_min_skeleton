d3f6d78ac69ef33699c524fbc9b1f9d7dabac6a3
submarrin <18marinam@gmail.com>
2019-11-28 00:03:14 +0500

    works but not correct

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ print(read_graph())
 def get_ribs(matr):
     ribs = []
     n = len(matr)
-    for i in range(0, n):
-        for j in range(0, n):
+    for j in range(0, n):
+        for i in range(0, j+1):
             if matr[i][j] is not None:
                 ribs.append([i, j, matr[i][j]])
     return ribs
@@ -36,12 +36,16 @@ def sort_ribs(ribs):
     return sorted(ribs, key=get_weight)
 
 
-def merge_components(graph_matrix, v, w, p_name, q_name):
-    n = len(graph_matrix)
-    name, next_comp, size = [None] * n, [None] * n, [None] * n
+matr = read_graph()
+ribs = get_ribs(matr)
+print(ribs)
+sorted_ribs = sort_ribs(ribs)
+print(sorted_ribs)
+
+
+def merge_components(v, w, p_name, q_name, name, next_comp, size):
     name[v] = p_name
     u = next_comp[v]
-    print(name, next_comp, size)
     while name[u] != p_name:
         name[u] = p_name
         u = next_comp[u]
@@ -50,29 +54,35 @@ def merge_components(graph_matrix, v, w, p_name, q_name):
     y = next_comp[w]
     next_comp[v] = y
     next_comp[w] = x
+    return name, next_comp, size
 
 
 def find_skeleton(graph_matrix):
     n = len(graph_matrix)
-    name, next_comp, size = [None]*n, [None]*n, [None]*n
+    name, next_comp, size = [None] * n, [None] * n, [None] * n
     queue_ribs = get_ribs(graph_matrix)
     queue_ribs = sort_ribs(queue_ribs)
     for i in range(n):
         name[i] = i
         next_comp[i] = i
         size[i] = 1
-    T = []
-    while len(T) != (n - 1):
+    # print(name, next_comp, size)
+    skeleton = []
+    while len(skeleton) != (n - 1):
         loc_rib = queue_ribs.pop(0)
         v = loc_rib[0]
         w = loc_rib[1]
         p_name = name[v]
         q_name = name[w]
+        print("p = ", p_name, "q = ", q_name)
         if p_name != q_name:
             if size[p_name] > size[q_name]:
-                merge_components(graph_matrix, w, v, q_name, p_name)
+                name, next_comp, size = merge_components(w, v, q_name, p_name, name, next_comp, size)
             else:
-                merge_components(graph_matrix, v, w, p_name, q_name)
+                name, next_comp, size = merge_components(v, w, p_name, q_name, name, next_comp, size)
+            new_rib = [v + 1, w + 1]
+            skeleton.append(new_rib)
+    return skeleton
 
 
 print(find_skeleton(read_graph()))
