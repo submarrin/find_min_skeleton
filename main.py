5bd7641c28447f4ed5dd72ff020698b77a34349e
submarrin <18marinam@gmail.com>
2019-11-27 20:33:11 +0500

    makes ribs and sorts them

--- main.py
+++ main.py
@@ -14,7 +14,56 @@ def read_graph():
         for index, weight in pairs:
             graph_matrix[i][int(index) - 1] = int(weight)
     file_in.close()
-    return (graph_matrix)
+    return graph_matrix
 
 print(read_graph())
+
+
+def sort_ribs(matr):
+    def get_third(item):
+        item[2]
+    make_ribs = []
+    n = len(matr)
+    for i in range(0, n):
+        for j in range(0, n):
+            if matr[i][j] is not None:
+                make_ribs.append({i, j, matr[i][j]})
+    return sorted(make_ribs, key=get_third)
+
+
+def merge_components(graph_matrix, v, w, p_name, q_name):
+    name, next_comp, size = []*len(graph_matrix)
+    name[v] = p_name
+    u = next_comp[v]
+    while name[u] != p_name:
+        name[u] = p_name
+        u = next_comp[u]
+    size[p_name] = size[p_name] + size[q_name]
+    x = next_comp[v]
+    y = next_comp[w]
+    next_comp[v] = y
+    next_comp[w] = x
+
+
+def find_skeleton(graph_matrix):
+    n = len(graph_matrix)
+    name, next_comp, size = []*len(graph_matrix)
+    queue_ribs = []
+    queue_ribs = sort_ribs(graph_matrix)
+    for i in range(n):
+        name[i] = i
+        next_comp[i] = i
+        size[i] = 1
+    T = []
+    while len(T) != (n - 1):
+        loc_rib = queue_ribs.pop(0)
+        v = loc_rib[0]
+        w = loc_rib[1]
+        p_name = name[v]
+        q_name = name[w]
+        if p_name != q_name:
+            if size[p_name] > size[q_name]:
+                merge_components(graph_matrix, w, v, q_name, p_name)
+            else:
+                merge_components(graph_matrix, v, w, p_name, q_name)
 
